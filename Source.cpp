397b61e5efe1fb1b7cf81295546687dbaba26f93
Maddy <37761862+Maddy54652@users.noreply.github.com>
2018-12-14 00:13:11 -0800

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include"cockroac.h"
+
+//I'm doing this for shits and giggles
+
+int main(){
+	printf("Guess which language I programmed this in!\n");
+	printf("Was it in fortran? Was it in java?\n");
+	printf("You know what?I made this in C...\n");
+	cout<<"SIKE! IT'S IN C++!! :D"<<endl;
+	return 0;
+}
+
+//https://www.youtube.com/watch?v=FJ3oHpup-pk
