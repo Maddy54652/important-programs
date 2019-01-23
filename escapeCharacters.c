ffb3bc4de744e455517740b2a3719d209ccdae49
Maddy <37761862+Maddy54652@users.noreply.github.com>
2019-01-23 10:42:04 -0800

    I made this! :D

---
+++ escapeCharacters.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(int argc,char* argv[]){
+
+	printf("escape characters....\n");
+	printf("\\b may be an escape character! \b \n");
+	printf("\\f may be as well... \f \n");
+	printf("\\n\n is\n a\n well\n known\n escape\n character.\n"); 
+	printf("So is \t \\t and \v \\v \n");
+	printf("\\\" is also an \"escape\" \"character\".\n");
+	printf("...escape characters...\n");
+	return 0;
+}
