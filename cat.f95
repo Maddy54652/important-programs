0799a85f4b74f94ef0ee61f351a0484e162e37fc
Maddy <37761862+Maddy54652@users.noreply.github.com>
2019-05-06 14:06:56 -0700

    Create cat.f95
    May need to be modified. I don't know how Fortran does new lines.

---
+++ cat.f95
@@ -0,0 +1,8 @@
+program cat
+	implicit none
+  print*,"                 _______           ZZZZ"
+  print*,"                / __    \  /\/\ ZZZZ"
+  print*,"               /    \    \/ uu \"
+  print*,"              / _____|_  > ____/<"
+  print*,"              \________)______))"
+end program cat
