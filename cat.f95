5b80f088782a189356b9f88e89e6500ce01a30ed
Maddy <37761862+Maddy54652@users.noreply.github.com>
2019-05-06 14:08:56 -0700

    Update cat.f95

--- cat.f95
+++ cat.f95
@@ -1,8 +1,9 @@
 program cat
+! this is supposed to be a drawing of a cat
 	implicit none
-  print*,"                 _______           ZZZZ"
-  print*,"                / __    \  /\/\ ZZZZ"
-  print*,"               /    \    \/ uu \"
-  print*,"              / _____|_  > ____/<"
-  print*,"              \________)______))"
+  	print*,"                 _______           ZZZZ"
+  	print*,"                / __    \  /\/\ ZZZZ"
+  	print*,"               /    \    \/ uu \"
+  	print*,"              / _____|_  > ____/<"
+  	print*,"              \________)______))"
 end program cat
