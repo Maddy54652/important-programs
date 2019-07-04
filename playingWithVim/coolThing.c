4bf0c2191d7d2d4c9fb83241910802e93ff0933b
Maddy <37761862+Maddy54652@users.noreply.github.com>
2019-07-04 16:43:42 -0700

    Create coolThing.c

---
+++ playingWithVim/coolThing.c
@@ -0,0 +1,10 @@
+//This is my first time using Vim
+//I have never used this before
+//It's the fourth of July!
+//Happy Birthday, America!
+#include<stdio.h>
+
+int main(int argc,char* argv[]){
+	printf("Hollah!\n");
+	return 0;
+}
