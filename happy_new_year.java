031a18ddf860619144e2c189b60a3d8a7536dad6
Maddy <37761862+Maddy54652@users.noreply.github.com>
2020-01-09 19:40:25 -0800

    Happy New Year!

---
+++ happy_new_year.java
@@ -0,0 +1,14 @@
+/************************************************/
+/* Programmer: Maddy Henderson                  */
+/*                                              */
+/* Program: happy_new_year.java                 */
+/*                                              */
+/* Approximate completion time: 8 minutes	*/
+/************************************************/
+
+public class happy_new_year{
+    public static void main(String[] args){
+	    System.out.println("Should auld acquaintance be forgot,and never brought to mind?");
+	    System.out.println("Should auld acquaintance be forgot,and auld lang syne?");
+        }
+}
